75fbdf9d9085b16b5f748e331a198e801a02d050
Michael Albert <michael.t.albert89@gmail.com>
2020-04-30 16:35:44 -0500

    updated seed.js
    Added semi colons

--- scripts/seed.js
+++ scripts/seed.js
@@ -20,38 +20,38 @@ for (var i = 0; i < 99; i ++) {
   } else if (i === 80) {
     shopId ++;
   }
-  var description = faker.fake("{{lorem.paragraphs}}")
-  var productName = faker.fake("{{commerce.productName}}")
-  var price = faker.fake("{{commerce.price}}")
+  var description = faker.fake("{{lorem.paragraphs}}");
+  var productName = faker.fake("{{commerce.productName}}");
+  var price = faker.fake("{{commerce.price}}");
 
-  var queryString = `INSERT INTO Products (name, shop_id, description, price) VALUES ("${productName}", ${shopId}, "${description}", "${price}")`
+  var queryString = `INSERT INTO Products (name, shop_id, description, price) VALUES ("${productName}", ${shopId}, "${description}", "${price}")`;
 
   connection.query(queryString, (err, results) => {
     if (err) {
-      console.error("There was an error: ", err)
+      console.error("There was an error: ", err);
     } else {
       productCount ++;
-      console.log("Successfully added 1 row to table")
+      console.log("Successfully added 1 row to table");
     }
   })
 }
 
 for (var i = 2; i < 6; i ++) {
-  var shopName = faker.fake("{{company.companyName}}")
-  var ownerName = faker.fake("{{name.firstName}} {{name.lastName}}")
-  var year = String(Math.floor(Math.random() * (2020 - 2006)) + 2006)
-  var totalSales = Math.floor(Math.random() * (500000 - 100)) + 100
-  var location = faker.fake("{{address.city}}, {{address.country}}")
-  var url = `https://shmetsy.s3.us-east-2.amazonaws.com/owner${i}.jpeg`
+  var shopName = faker.fake("{{company.companyName}}");
+  var ownerName = faker.fake("{{name.firstName}} {{name.lastName}}");
+  var year = String(Math.floor(Math.random() * (2020 - 2006)) + 2006);
+  var totalSales = Math.floor(Math.random() * (500000 - 100)) + 100;
+  var location = faker.fake("{{address.city}}, {{address.country}}");
+  var url = `https://shmetsy.s3.us-east-2.amazonaws.com/owner${i}.jpeg`;
 
-  var queryString = `INSERT INTO Shops (shop_name, owner_name, location, total_sales, founded_date, owner_url) VALUES ("${shopName}", "${ownerName}", "${location}", ${totalSales}, "${year}", "${url}")`
+  var queryString = `INSERT INTO Shops (shop_name, owner_name, location, total_sales, founded_date, owner_url) VALUES ("${shopName}", "${ownerName}", "${location}", ${totalSales}, "${year}", "${url}")`;
 
   connection.query(queryString, (err, results) => {
     if (err) {
-      console.error("There was an error: ", err)
+      console.error("There was an error: ", err);
     } else {
       shopCount ++;
-      console.log("Successfully added 1 row to table")
+      console.log("Successfully added 1 row to table");
     }
   })
 }
@@ -60,19 +60,19 @@ for (var i = 2; i < 6; i ++) {
 for (var i = 1; i < 101; i ++) {
   for (var j = 0; j < 5; j ++) {
     var color = faker.fake("{{commerce.productAdjective}} {{commerce.color}}");
-    var mod = Math.floor(Math.random() * (200 - 1) + 1)
-    var queryString = `INSERT INTO Colors (color_name, price_modifier, product_id) VALUES ("${color}", ${mod}, ${i})`
+    var mod = Math.floor(Math.random() * (200 - 1) + 1);
+    var queryString = `INSERT INTO Colors (color_name, price_modifier, product_id) VALUES ("${color}", ${mod}, ${i})`;
 
     connection.query(queryString, (err, results) => {
       if (err) {
-        console.error("There was an error: ", err)
+        console.error("There was an error: ", err);
       } else {
         colorCount ++;
-        console.log("Successfully added 1 row to table")
+        console.log("Successfully added 1 row to table");
       }
     })
   }
 }
 
-connection.end()
+connection.end();
 
