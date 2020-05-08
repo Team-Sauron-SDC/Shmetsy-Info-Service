7dfe70d449b49a8b08396c64f75c393bbbcd4195
mta630 <michael.t.albert89@gmail.com>
2020-05-08 11:46:40 -0500

    Finished header

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import Header from './components/Header.jsx';
 
 
+ReactDOM.render(<Header />, document.getElementById('header'));
 ReactDOM.render(<App />, document.getElementById('app'));
