5544cde6a1340a23e96564bd6f2bedcf8d9ff2a7
Jacob Orel <jakeorel@gmail.com>
2020-05-20 17:32:25 -0500

    added post, put, and delete queries

--- web/DB/index.js
+++ web/DB/index.js
@@ -65,8 +65,47 @@ const getShop = (id, callback) => {
   });
 };
 
+const deleteItem = (name, callback) => {
+  const queryString = `DELETE FROM Products WHERE name = '${name}'`;
+
+  connection.query(queryString, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+}
+
+const addProduct = (body, callback) => {
+  const queryString = `INSERT INTO Products (name, shop_id, description, price, rating) VALUES ('${body.name}', '${body.shop_id}', '${body.description}', '${body.price}', '${body.rating}')`;
+
+  connection.query(queryString, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+}
+
+const updateSeller = (body, shopId, callback) => {
+  const queryString = `UPDATE Shops SET shop_name = '${body.shop_name}', owner_name = '${body.owner_name}', location = '${body.location}' WHERE id = '${shopId}'`;
+
+  connection.query(queryString, (err, result) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, result);
+    }
+  });
+}
+
 module.exports = {
   getProduct,
   getShop,
   getColors,
+  deleteItem,
+  addProduct,
+  updateSeller,
 }
