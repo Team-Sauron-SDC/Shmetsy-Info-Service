d78703ab3f15f832bfb96e1c049d0875edceb2d4
Jacob Orel <jakeorel@gmail.com>
2020-05-23 19:26:34 -0500

    wrote function for get requests

--- web/DB/Cassandra.js
+++ web/DB/Cassandra.js
@@ -22,14 +22,14 @@ tempClient.connect()
     client.connect((err) => (err ? console.log('failed to connect', err) : console.log('connect to newkeyspace')));
   })
   .then(() => {
-    const createTable = 'CREATE TABLE IF NOT EXISTS product_info.products (id int PRIMARY KEY, name text, description text, price double, rating text, shop_name text, owner_name text, totalSales int, location text, owner_url text, colors text)';
+    const createTable = 'CREATE TABLE IF NOT EXISTS product_info.products (id int PRIMARY KEY, name text, description text, price double, rating text, shop_name text, owner_name text, total_sales int, location text, owner_url text, colors text)';
 
     return client.execute(createTable);
   })
   .catch((err) => console.log('connection err: ', err));
 
 const getProduct = (id, callback) => {
-  const queryStr = `SELECT name, description, price, rating FROM product_info.products WHERE id = 1`;
+  const queryStr = `SELECT name, description, price, rating FROM product_info.products WHERE id = ${id}`;
 
   client.execute(queryStr, (err, res) => {
     if (err) {
@@ -38,8 +38,34 @@ const getProduct = (id, callback) => {
       callback(null, res);
     }
   });
-  };
+};
 
-  module.exports = {
-    getProduct,
-  }
+const getShop = (id, callback) => {
+  const queryStr = `SELECT shop_name, owner_name, total_sales, location, owner_url FROM products WHERE id = ${id}`;
+
+  client.execute(queryStr, (err, res) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  });
+};
+
+const getColors = (id, callback) => {
+  const queryStr = `SELECT colors FROM products WHERE id = ${id}`;
+
+  client.execute(queryStr, (err, res) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      callback(null, res);
+    }
+  });
+};
+
+module.exports = {
+  getProduct,
+  getShop,
+  getColors,
+};
