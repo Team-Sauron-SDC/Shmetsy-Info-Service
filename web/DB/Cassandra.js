13dc7380fe3545e63f58b498c56dfdca79828f77
Jacob Orel <jakeorel@gmail.com>
2020-05-26 18:08:02 -0500

    moved query funcs to models folder

--- web/DB/Cassandra.js
+++ web/DB/Cassandra.js
@@ -28,44 +28,4 @@ tempClient.connect()
   })
   .catch((err) => console.log('connection err: ', err));
 
-const getProduct = (id, callback) => {
-  const queryStr = `SELECT name, description, price, rating FROM product_info.products WHERE id = ${id}`;
-
-  client.execute(queryStr, (err, res) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, res);
-    }
-  });
-};
-
-const getShop = (id, callback) => {
-  const queryStr = `SELECT shop_name, owner_name, total_sales, location, owner_url FROM products WHERE id = ${id}`;
-
-  client.execute(queryStr, (err, res) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, res);
-    }
-  });
-};
-
-const getColors = (id, callback) => {
-  const queryStr = `SELECT colors FROM products WHERE id = ${id}`;
-
-  client.execute(queryStr, (err, res) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, res);
-    }
-  });
-};
-
-module.exports = {
-  getProduct,
-  getShop,
-  getColors,
-};
+module.exports = client;
