fe467eaf58873055874163d8f4fef194c7536a12
Jacob Orel <jakeorel@gmail.com>
2020-05-29 18:42:10 -0500

    moved postgres funcs to top

--- web/server/index.js
+++ web/server/index.js
@@ -1,7 +1,7 @@
+// eslint-disable-next-line no-unused-vars
 const nr = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
 const compression = require('compression');
 // const Mysql = require('./controllers/mysql.js');
 const Postgres = require('./controllers/postgres.js');
@@ -10,15 +10,11 @@ const Postgres = require('./controllers/postgres.js');
 const app = express();
 
 app.use(compression());
-app.use(express.static(path.join(__dirname, '/../public')));
-app.use('/:id', express.static(path.join(__dirname, '/../public')));
+app.use(express.static('public'));
+app.use('/:id', express.static('public'));
 app.use(bodyParser.json());
 
 const PORT = 3001;
-
-app.get('/:id/undefined', (req, res) => {
-  res.sendStatus(200);
-});
 
 /* POSTGRES */
 app.get('/product/:id', Postgres.getProduct);
@@ -27,7 +23,7 @@ app.get('/product/colors/:id', Postgres.getColors);
 
 app.get('/product/shop/:shopId', Postgres.getShop);
 
-app.post('/product/:id', Postgres.addProduct);
+app.post('/product/1', Postgres.addProduct);
 
 /* MYSQL */
 // app.get('/product/:id', Mysql.getProduct);
