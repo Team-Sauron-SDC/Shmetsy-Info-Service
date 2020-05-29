02b49083f248feeae4e5d75b1fcd19177f2fda1d
Jacob Orel <jakeorel@gmail.com>
2020-05-29 18:41:44 -0500

    removed debug console logs

--- web/server/controllers/postgres.js
+++ web/server/controllers/postgres.js
@@ -7,26 +7,26 @@ exports.getColors = ((req, res) => {
       const colors = JSON.parse(results[0].colors.replace(/'/g, '"'));
       res.send(colors);
     })
-    .catch((err) => res.send(err.name));
+    .catch((err) => res.sendStatus(400));
 });
 
 exports.getProduct = (req, res) => {
   const id = Number(req.params.id);
   Postgres.getProduct(id)
     .then((result) => res.send(result))
-    .catch((err) => res.sendStatus(400).send(err.name));
+    .catch((err) => res.sendStatus(400));
 };
 
 exports.getShop = (req, res) => {
   const id = Number(req.params.shopId);
   Postgres.getShop(id)
     .then((result) => res.send(result))
-    .catch((err) => res.sendStatus(400).send(err.name));
+    .catch((err) => res.sendStatus(400));
 };
 
 exports.addProduct = (req, res) => {
   const newProduct = req.body;
   Postgres.addShop(newProduct)
     .then((result) => res.send(result))
-    .catch((err) => res.sendStatus(500).send(err.name));
+    .catch((err) => res.sendStatus(400));
 };
